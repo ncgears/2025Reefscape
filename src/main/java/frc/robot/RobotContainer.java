d587525f6e80e8a9cdf61836f7a19a3189700db1
ncgears <1918ncgears@gmail.com>
2025-01-06 19:32:11 -0500

    Initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,91 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandStadiaController;
+import frc.robot.constants.*;
+import frc.robot.utils.InputAxis;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class RobotContainer {
+    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    private double MaxAngularRate = RotationsPerSecond.of(SwerveConstants.kMaxAngularRate).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+    private final Telemetry logger = new Telemetry(MaxSpeed);
+    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+    /* Setting up bindings for necessary control of the swerve drive platform */
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+        .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+    private final CommandStadiaController dj = new CommandStadiaController(OIConstants.JoyDriverID);
+
+    public RobotContainer() {
+        final InputAxis m_fieldX = new InputAxis("Forward", dj::getLeftY)
+            .withDeadband(OIConstants.kMinDeadband)
+            .withInvert(true)
+            .withSquaring(true);
+        final InputAxis m_fieldY = new InputAxis("Strafe", dj::getLeftX)
+            .withDeadband(OIConstants.kMinDeadband)
+            .withInvert(true)
+            .withSquaring(true);
+        final InputAxis m_rotate = new InputAxis("Rotate", dj::getRightX)
+            .withDeadband(OIConstants.kMinDeadband)
+            .withInvert(true);
+        // final InputAxis m_angleX = new InputAxis("Angle X", dj::getRightX);
+        // final InputAxis m_angleY = new InputAxis("Angle Y", dj::getRightY);
+
+        configureBindings();
+
+        drivetrain.setDefaultCommand(
+            // Drivetrain will execute this command periodically
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(m_fieldX.getAsDouble() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(m_fieldY.getAsDouble() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(m_rotate.getAsDouble() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            )
+        );
+    }
+
+    private void configureBindings() {
+        // dj.frame().onTrue((Commands.runOnce(drivetrain::zeroGyro)));
+        // dj.stadia().onTrue(Commands.runOnce(drivetrain::addFakeVisionReading));
+
+        //hold A to apply brake
+        dj.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        //hold B to point the wheels forward
+        dj.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-dj.getLeftY(),-dj.getLeftX()))
+        ));
+
+        // Run SysId routines when holding ellipses/google and X/Y.
+        // Note that each routine should be run exactly once in a single log.
+        dj.ellipses().and(dj.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        dj.ellipses().and(dj.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        dj.google().and(dj.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        dj.google().and(dj.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+        // reset the field-centric heading on frame button press
+        dj.frame().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+
+        drivetrain.registerTelemetry(logger::telemeterize);
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+        // return drivetrain.getAutonomousCommand("New Auto");
+    }
+}

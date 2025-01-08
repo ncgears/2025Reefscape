8c5b4996043b91098a61b11c01d6348ae8b54a91
ncgears <1918ncgears@gmail.com>
2025-01-08 13:22:50 -0500

    Suppress warnings in drivetrain subsystem

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -28,6 +28,7 @@ import frc.robot.constants.TunerConstants.TunerSwerveDrivetrain;
  * Class that extends the Phoenix 6 SwerveDrivetrain class and implements
  * Subsystem so it can easily be used in command-based projects.
  */
+@SuppressWarnings({"unused"})
 public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Subsystem {
     private static final double kSimLoopPeriod = 0.005; // 5 ms
     private Notifier m_simNotifier = null;

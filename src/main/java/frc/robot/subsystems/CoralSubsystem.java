4d2e351ff08815cf7dfa8629e1b7e3cc90a509f3
ncgears <1918ncgears@gmail.com>
2025-02-20 11:03:47 -0500

    Region-ify Coral subsys

--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -1,13 +1,17 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Volts;
+
 import java.util.Map;
 
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.StaticBrake;
-import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.controls.VoltageOut;
+// import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.filter.LinearFilter;
@@ -15,8 +19,11 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.*; 
 import frc.robot.utils.NCDebug;
 import frc.robot.RobotContainer;
@@ -28,6 +35,7 @@ import frc.robot.RobotContainer;
 public class CoralSubsystem extends SubsystemBase {
 	private static CoralSubsystem instance;
   //private and public variables defined here
+  //#region Declarations
   public enum Direction {
     OUT(DashboardConstants.Colors.GREEN),
     IN(DashboardConstants.Colors.RED),
@@ -44,14 +52,18 @@ public class CoralSubsystem extends SubsystemBase {
   private final NeutralOut m_neutral = new NeutralOut();
   private final StaticBrake m_brake = new StaticBrake();
 
-  private final CANcoder m_encoder;
-  private final TalonFX m_motor1;
+  // private final CANcoder m_encoder;
+  private TalonFX m_motor1;
   private final LinearFilter curSpikeFilter = LinearFilter.highPass(0.1, 0.02);
   private static final double curSpikeLimit = CoralConstants.kCurrentSpikeLimit;
+  //#endregion Declarations
 
+  //#region Triggers
   private final Trigger curSpikeTrigger = new Trigger(() -> curSpikeFilter.calculate(getStatorCurrent()) > curSpikeLimit).debounce(0.15);
   public final Trigger isRunning = new Trigger(() -> { return (m_curDirection != Direction.STOP);});
+  //#endregion Triggers
 
+  //#region Setup
   /**
 	 * Returns the instance of the CoralSubsystem subsystem.
 	 * The purpose of this is to only create an instance if one does not already exist.
@@ -71,8 +83,8 @@ public class CoralSubsystem extends SubsystemBase {
     // m_config.apply(m_fxsConfigs);
 
     //initialize values for private and public variables, etc.
-    m_encoder = new CANcoder(CoralConstants.kCANcoderID, CoralConstants.canBus);
-    RobotContainer.ctreConfigs.retryConfigApply(()->m_encoder.getConfigurator().apply(RobotContainer.ctreConfigs.coralCCConfig));
+    // m_encoder = new CANcoder(CoralConstants.kCANcoderID, CoralConstants.canBus);
+    // RobotContainer.ctreConfigs.retryConfigApply(()->m_encoder.getConfigurator().apply(RobotContainer.ctreConfigs.coralCCConfig));
 
     m_motor1 = new TalonFX(CoralConstants.kMotorID, CoralConstants.canBus);
     RobotContainer.ctreConfigs.retryConfigApply(()->m_motor1.getConfigurator().apply(RobotContainer.ctreConfigs.coralFXConfig));
@@ -93,7 +105,9 @@ public class CoralSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
   }
+  //#endregion Setup
 
+  //#region Dashboard
   public void createDashboards() {
     ShuffleboardTab driverTab = Shuffleboard.getTab("Driver");
     driverTab.addString("Coral", this::getColor)
@@ -126,15 +140,9 @@ public class CoralSubsystem extends SubsystemBase {
       //   .withProperties(Map.of("show_type",false));  
     }
   }
+  //#endregion Dashboard
 
-  public void coralStop() {
-    m_motor1.setControl(m_neutral);
-    if(m_curDirection != Direction.HOLD) {
-      m_curDirection = Direction.STOP;
-      NCDebug.Debug.debug("Coral: Stop");
-    }
-  }
-
+  //#region Getters
   public Direction getDirection() { return m_curDirection; }
   public String getDirectionName() { return m_curDirection.toString(); }
   public String getColor() { return m_curDirection.getColor(); }
@@ -147,4 +155,61 @@ public class CoralSubsystem extends SubsystemBase {
     TalonFX[] motors = {m_motor1};
     return motors;
   }
+  //#endregion Getters
+
+  //#region Setters
+  //#endregion Setters
+
+  //#region Limits
+  public boolean getForwardLimit() {
+    return m_motor1.getPosition().getValueAsDouble() >= CoralConstants.Positions.kFwdLimit;
+  }
+  public boolean getReverseLimit() {
+    return m_motor1.getPosition().getValueAsDouble() <= CoralConstants.Positions.kRevLimit;
+  }
+  public boolean atLimit() {
+    return getForwardLimit() || getReverseLimit();
+  }
+  //#endregion Limits
+  
+  //#region Controls
+  public void coralStop() {
+    m_motor1.setControl(m_neutral);
+    if(m_curDirection != Direction.HOLD) {
+      m_curDirection = Direction.STOP;
+      NCDebug.Debug.debug("Coral: Stop");
+    }
+  }
+  //#endregion Controls
+
+  //#region SysID Functions
+  private final VoltageOut m_voltReq = new VoltageOut(0.0);
+  private final SysIdRoutine m_sysIdRoutine = new SysIdRoutine(
+    new SysIdRoutine.Config(
+      null, //default ramp rate 1V/s
+      Volts.of(4), //reduce dynamic step voltage to 4 to prevent brownout
+      null, //default timeout 10s
+      (state) -> SignalLogger.writeString("state", state.toString())
+    ),
+    new SysIdRoutine.Mechanism(
+      (volts) -> m_motor1.setControl(m_voltReq.withOutput(volts.in(Volts))),
+      null,
+      this
+    )
+  );
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+      return m_sysIdRoutine.quasistatic(direction);
+  }
+  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+      return m_sysIdRoutine.dynamic(direction);
+  }
+  public Command runSysIdCommand() {
+    return Commands.sequence(
+      sysIdQuasistatic(SysIdRoutine.Direction.kForward).until(this::atLimit),
+      sysIdQuasistatic(SysIdRoutine.Direction.kReverse).until(this::atLimit),
+      sysIdDynamic(SysIdRoutine.Direction.kForward).until(this::atLimit),
+      sysIdDynamic(SysIdRoutine.Direction.kReverse).until(this::atLimit)
+    );
+  }
+  //#endregion SysID Functions
 }

761489f4e4ae06ee909f56e097b9b94293dbdb43
ncgears <1918ncgears@gmail.com>
2025-02-22 12:38:44 -0500

    Update device IDs

--- src/main/java/frc/robot/constants/ID.java
+++ src/main/java/frc/robot/constants/ID.java
@@ -47,14 +47,14 @@ public class ID {
      * IDs of Krakens/Falcons
      */
     public static final class TalonFX {
-        public static int swerve_fl_drive = 7;
-        public static int swerve_fr_drive = 8;
-        public static int swerve_bl_drive = 6;
-        public static int swerve_br_drive = 5;
-        public static int swerve_fl_turn = 3;
-        public static int swerve_fr_turn = 4;
-        public static int swerve_bl_turn = 2;
-        public static int swerve_br_turn = 1;
+        public static int swerve_fl_drive = 8;
+        public static int swerve_fr_drive = 5;
+        public static int swerve_bl_drive = 7;
+        public static int swerve_br_drive = 6;
+        public static int swerve_fl_turn = 1;
+        public static int swerve_fr_turn = 3;
+        public static int swerve_bl_turn = 4;
+        public static int swerve_br_turn = 2;
         public static int climber = 9;
         public static int coral = 10;
         public static int algae_wrist = 11;
@@ -64,8 +64,8 @@ public class ID {
      * IDs of TalonFXS
      */
     public static final class TalonFXS {
-        public static int algae_left = 12;
-        public static int algae_right = 13;
+        public static int algae_left = 34;
+        public static int algae_right = 33;
     }
     /**
      * IDs of CANdles
@@ -82,9 +82,9 @@ public class ID {
         public static int swerve_fr_cc = 4;
         public static int swerve_bl_cc = 2;
         public static int swerve_br_cc = 1;
-        public static int coral = 20;
-        public static int algae = 21;
-        public static int climber = 22;
-        public static int elevator = 23;
+        // public static int coral = 14;
+        public static int algae = 25;
+        // public static int climber = 15;
+        public static int elevator = 22;
     }
 }

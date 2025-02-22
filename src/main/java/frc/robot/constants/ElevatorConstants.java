8c010480cf9d7d22e149d2e8b9a5a22141e09c7f
ncgears <1918ncgears@gmail.com>
2025-02-22 15:09:10 -0500

    Elevator tuning

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -21,7 +21,7 @@ public class ElevatorConstants {
     public static final double kMagnetOffset = 1.690918; //Adjust magnet to sensor offset for CANcoder
     public static final int kMotorID = ID.TalonFX.elevator;
     public static final boolean kIsInverted = true;
-    public static final NeutralModeValue kNeutralMode = NeutralModeValue.Coast;
+    public static final NeutralModeValue kNeutralMode = NeutralModeValue.Brake;
     public static final double kPositionTolerance = 0.05; //how close to be considered "at target"
 
     public static final double kGearRatio = 9; // 20:1 gearbox (0.05), 18t:42t -- this is between rotor and sensor

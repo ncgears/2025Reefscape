6eaf74886cf13a6dd5765a4dd5923bf238bb3c6a
ncgears <1918ncgears@gmail.com>
2025-03-05 20:28:29 -0500

    Reduce coral kP

--- src/main/java/frc/robot/constants/CoralConstants.java
+++ src/main/java/frc/robot/constants/CoralConstants.java
@@ -31,7 +31,7 @@ public class CoralConstants {
     public static final double kS = 0.3; // add kS to overcome static friction: adjust first to start moving
     public static final double kV = 1.75; // add kV for velocity target: voltage(12) / velocity target.. 1 rps results in 0.12v output
     public static final double kA = 0.0; // add kA for acceleration: 0.01 = 1 rps/s requires 0.01v output
-    public static final double kP = 120.0; // add kP per rotation of error: error of 1 rotation results in 12v output (this might be low for aimer)
+    public static final double kP = 2.0; // add kP per rotation of error: error of 1 rotation results in 12v output (this might be low for aimer)
     public static final double kI = 0.01; // no integral
     public static final double kD = 0.0; // 0.1 = velocity error of 1rps results in 0.1v output
     public static final double kMotionMagicCruise = 30; // Motor Max / Gear Ratio

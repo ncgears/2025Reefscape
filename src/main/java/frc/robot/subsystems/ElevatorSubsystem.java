1c905ce36ae492215970e82ed26628b62db2da8a
ncgears <1918ncgears@gmail.com>
2025-03-19 09:50:16 -0400

    Remove discreet PID slots for elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -252,9 +252,10 @@ public class ElevatorSubsystem extends SubsystemBase {
   public void setPosition(Position position) {
     setPrevPosition(position);
     m_targetPosition = position;
-    //if down, use slot1; if up use slot0
-    int slot = (position.getRotations() > m_motor1.getClosedLoopReference().getValueAsDouble()) ? 1 : 0; 
-    m_motor1.setControl(m_mmVoltage.withPosition(position.getRotations()).withSlot(slot));
+    // //if down, use slot1; if up use slot0
+    // int slot = (position.getRotations() > m_motor1.getClosedLoopReference().getValueAsDouble()) ? 1 : 0; 
+    // m_motor1.setControl(m_mmVoltage.withPosition(position.getRotations()).withSlot(slot));
+    m_motor1.setControl(m_mmVoltage.withPosition(position.getRotations()));
     NCDebug.Debug.debug("Elevator: Move to " + position.toString());
   }
   // #endregion Setters
